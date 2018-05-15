0a7cd0ed74101fc483130335833db49ed49b78ee
Unknown <simonphilip42@gmail.com>
2018-05-15 18:29:32 -0300

    Assignment 1 Submission
    Updated csci3130_demo for Assignment 1. Added a button and text input that combine to change the original displayed text.

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView textView = findViewById(R.id.textView);
+        textView.setText("Hello World!");
+
+        final Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener(){
+          public void onClick(View v) {
+              final EditText input = findViewById(R.id.editText);
+              String textInput = input.getText().toString();
+
+              final TextView textView = findViewById(R.id.textView);
+              textView.setText(textInput);
+          }
+        });
     }
 }
